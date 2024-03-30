3a5baa85080df8216fd1c7f48bcb8dbe0af8f76f
Mohammed Alrushdy <mohammed_alrushdy@yahoo.com>
2024-03-30 16:01:36 +0300

    Server Actions

--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -1,6 +1,30 @@
+import { redirect } from 'next/navigation';
+import { db } from '@/db';
+
 export default function SnippetCreatePage() {
+  async function createSnippet(formData: FormData) {
+    // This needs tobe a server action
+    'use server';
+
+    // check a user's inputs and make sure they're valid
+    const title = formData.get('title') as string;
+    const code = formData.get('code') as string;
+
+    // Create a new record in the database.
+    const snippet = await db.snippet.create({
+      data: {
+        title,
+        code,
+      },
+    });
+    console.log(snippet);
+
+    // Redirect the user to the home page.
+    redirect('/');
+  }
+
   return (
-    <form>
+    <form action={createSnippet}>
       <h3 className='font-bold m-3'>Create a Snippet</h3>
       <div className='flex flex-col gap-4'>
         <div className='flex gap-4'>
