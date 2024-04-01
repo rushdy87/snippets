e76f484df793526b93af209d3af9dd6e72942dfa
Mohammed Alrushdy <mohammed_alrushdy@yahoo.com>
2024-04-01 04:12:41 +0300

    Fetching Particular Records

--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -1,5 +1,19 @@
-export default function SnippetShowPage(props: any) {
-  console.log(props.params.id);
+import { notFound } from 'next/navigation';
+import { db } from '@/db';
 
-  return <div>Show Snippet</div>;
+interface SnippetShowPage {
+  params: {
+    id: String;
+  };
+}
+
+export default async function SnippetShowPage(props: SnippetShowPage) {
+  const { id } = props.params;
+  const snippet = await db.snippet.findFirst({ where: { id: Number(id) } });
+
+  if (!snippet) {
+    return notFound();
+  }
+
+  return <div>{snippet?.title}</div>;
 }
