261edaf0a0f17af55aec896aabf3a97e9631a51b
Mohammed Alrushdy <mohammed_alrushdy@yahoo.com>
2024-04-01 03:56:24 +0300

    Fetching Data with Server Components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
-export default function Home() {
-  return <div>Home page</div>;
+import { db } from '@/db';
+export default async function Home() {
+  const snippets = await db.snippet.findMany();
+
+  const renderSnippets = snippets.map((snippet) => {
+    return <div key={snippet.id}>{snippet.title}</div>;
+  });
+
+  return <div>{renderSnippets}</div>;
 }
